5508a732e7787892645654636b280374270acf9d
Thanh Tran <thanh.tran2@stunited.vn>
2022-04-04 01:10:57 +0700

    MVC(Refactor code) Router & Controller

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ const path = require("path")
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 //HTTP Logger
 app.use(morgan("combined"));
@@ -17,14 +24,8 @@ app.engine('hbs', handlebars.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get("/", (req, res) => {
-  console.log(__dirname)
-  res.render('home');
-});
-
-app.get("/news", (req, res) => {
-  res.render('news');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
