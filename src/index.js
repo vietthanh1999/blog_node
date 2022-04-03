2609db4ee59c1515104a2777ca2dd0c7b9ae7c9b
Thanh Tran <thanh.tran2@stunited.vn>
2022-04-03 22:52:46 +0700

    template engines (handlebars)

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const handlebars  = require('express-handlebars');
+const path = require("path")
+const app = express();
+const port = 3000;
+
+//HTTP Logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine('hbs', handlebars.engine(
+  { extname: '.hbs', defaultLayout: "main"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+app.get("/", (req, res) => {
+  res.render('home');
+});
+
+app.get("/news", (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
